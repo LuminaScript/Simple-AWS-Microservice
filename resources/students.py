2ea49fdd3ff015f8c5a9c355a9b38b4aa271a791
yizhencode <zhangyizhenyizen@gmail.com>
2023-09-20 21:39:17 -0400

    init

--- resources/students.py
+++ resources/students.py
@@ -1,3 +1,11 @@
+'''
+Author: yizhencode zhangyizhenyizen@gmail.com
+Date: 2023-09-20 21:21:46
+LastEditors: yizhencode zhangyizhenyizen@gmail.com
+LastEditTime: 2023-09-20 21:33:04
+FilePath: /6156_hw1/e6156-microservice-1/resources/students.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import json
 
 
@@ -8,7 +16,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/yizhenzhang/Desktop/6156_hw1/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
